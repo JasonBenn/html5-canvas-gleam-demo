bb0bff51855ac6159fd9d3c112bd315caf904e0c
Jason Benn <jasoncbenn@gmail.com>
2013-09-15 08:30:58 -0700

    Abstract letterSpacing variable.

--- canvas.js
+++ canvas.js
@@ -5,25 +5,26 @@ ctx.font = '4em Helvetica' // Maven Pro
 var hue = 0,
     i   = 0,
     phrase = 'CLINKLE',
-    initialXPos = ((ctx.canvas.width - (ctx.measureText(phrase).width + (phrase.length - 1) * 25)) / 2),
+    letterSpacing = 20,
+    initialXPos = ((ctx.canvas.width - (ctx.measureText(phrase).width + (phrase.length - 1) * letterSpacing)) / 2),
     length = phrase.length,
     height = ctx.canvas.height/2;
 
 ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-blackOut()
+drawBackground()
 ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
 
 setInterval(function() {
   gleam();
 }, 2500)
 
-setInterval(blackOut, 150)
+setInterval(drawBackground, 150)
 
 function gleam() {
   var xPos = initialXPos
   for (var n = 0; n < length; n++) {
     setTimeout(draw.bind(null, phrase[n], xPos, height), 100 * n)
-    xPos += ctx.measureText(phrase[n]).width + 25
+    xPos += ctx.measureText(phrase[n]).width + letterSpacing
   }
 }
 
@@ -37,7 +38,7 @@ function draw(text, x, y) {
   ctx.shadowBlur = 0;
 }
 
-function blackOut() {
+function drawBackground() {
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   ctx.lineWidth = 1
   ctx.strokeStyle = 'grey'
@@ -45,6 +46,6 @@ function blackOut() {
   var xPos = initialXPos
   phrase.split('').forEach(function(letter, index) {
     ctx.strokeText(letter, xPos, height);
-    xPos += ctx.measureText(letter).width + 25
+    xPos += ctx.measureText(letter).width + letterSpacing
   })
 }
